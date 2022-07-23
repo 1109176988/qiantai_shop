008418522fb3980458dde356cc791ead9f83c5e7
Teacher Egg <fury_liu@sina.com>
2022-07-23 16:12:47 +0800

    创建vue项目并且增加了poblic的css和font文件

---
+++ notes.md
@@ -0,0 +1,3 @@
+项目开始 进行npm run build 项目打包
+项目打包后安装serve 判断有没有serve打开cmd 输入serve 如果有会显示 没有的话在cmd npm install serve -g 进行全局安装
+serve全局安装后 打开项目控制台 serve dist 就可以运行打包文件 可以看见运行后的url 浏览器打开url就可以了 打包的目的是上线
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //引入vue
+import App from './App.vue' //引入app组件
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false //设置控制台中默认不显示提示信息 warning
 
 new Vue({
   router,
